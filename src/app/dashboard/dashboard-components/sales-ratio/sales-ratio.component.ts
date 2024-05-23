1803cd465d710145a954f0ee8b7a33de07b84627
Ismail448 <itsisma448@gmail.com>
2024-05-23 13:06:58 +0200

    dashboard

--- src/app/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
+++ src/app/dashboard/dashboard-components/sales-ratio/sales-ratio.component.ts
@@ -1,102 +1,74 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import {
-  ApexAxisChartSeries,
-  ApexChart,
-  ChartComponent,
-  ApexDataLabels,
-  ApexYAxis,
-  ApexLegend,
-  ApexXAxis,
-  ApexTooltip,
-  ApexTheme,
-  ApexGrid,
-  ApexPlotOptions,
-  ApexStroke
-
-  // ChartComponent,
-  // ApexAxisChartSeries,
-  // ApexChart,
-  // ApexXAxis,
-  // ApexDataLabels,
-  // ApexTooltip,
-  // ApexStroke
-} from 'ng-apexcharts';
-
-export type salesChartOptions = {
-  series: ApexAxisChartSeries | any;
-  chart: ApexChart | any;
-  xaxis: ApexXAxis | any;
-  yaxis: ApexYAxis | any;
-  stroke: any;
-  theme: ApexTheme | any;
-  tooltip: ApexTooltip | any;
-  dataLabels: ApexDataLabels | any;
-  legend: ApexLegend | any;
-  colors: string[] | any;
-  markers: any;
-  grid: ApexGrid | any;
-  plotOptions: ApexPlotOptions | any;
-};
+import { Component, OnInit } from '@angular/core';
+import { Coche } from 'src/app/pages/apps/coche/interfaces/Coche.model';
+import { CocheService } from 'src/app/pages/apps/coche/services/coche.service';
 
 @Component({
   selector: 'app-sales-ratio',
-  templateUrl: './sales-ratio.component.html'
+  templateUrl: './sales-ratio.component.html',
+  styleUrls: []
 })
 export class SalesRatioComponent implements OnInit {
+  coches: Coche[] = [];
+  salesChartOptions: any = {};
 
-  @ViewChild("chart") chart: ChartComponent = Object.create(null);
-  public salesChartOptions: Partial<salesChartOptions>;
-  constructor() {
+  constructor(private cocheService: CocheService) { }
+
+  ngOnInit(): void {
+    this.getCoches();
+  }
+
+  getCoches(): void {
+    this.cocheService.getCoches().subscribe(coches => {
+      this.coches = coches;
+      this.updateChartOptions();
+    });
+  }
+
+  updateChartOptions(): void {
     this.salesChartOptions = {
-      series: [
-        {
-          name: "series1",
-          data: [31, 40, 28, 51, 42, 109, 100]
-        },
-        {
-          name: "series2",
-          data: [11, 32, 45, 32, 34, 52, 41]
-        }
-      ],
+      series: [{
+        name: 'Price',
+        data: this.coches.map(coche => coche.precio)
+      }],
       chart: {
-        fontFamily: 'Montserrat,sans-serif',
-        height: 290,
-        type: 'area',
-        toolbar: {
-          show: false
-        },
+        height: 350,
+        type: 'bar'
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '55%',
+          endingShape: 'rounded'
+        }
       },
       dataLabels: {
         enabled: false
       },
-      colors: ["#0d6efd", "#009efb", "#6771dc"],
       stroke: {
         show: true,
-        width: 4,
-        colors: ["transparent"],
-      },
-      grid: {
-        strokeDashArray: 3,
+        width: 2,
+        colors: ['transparent']
       },
       xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July",
-          "Aug",
-        ],
+        categories: this.coches.map(coche => coche.nombreModelo)
+      },
+      yaxis: {
+        title: {
+          text: 'Price (€)'
+        }
+      },
+      fill: {
+        opacity: 1
       },
       tooltip: {
-        theme: 'dark'
+        y: {
+          formatter: function (val: string) {
+            return "€ " + val;
+          }
+        }
       }
     };
   }
-
-  ngOnInit(): void {
-  }
-
 }
+
+
